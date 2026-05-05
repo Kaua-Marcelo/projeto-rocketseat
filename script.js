59d6639b0ecace52ccc4dc4524f13f71f5a50753
Kauã <kauamarcelo003@gmail.com>
2026-05-05 16:28:44 -0300

    adicionei minhas fotos no avatar e alterei o @ da página

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', 'https://media.licdn.com/dms/image/v2/D4D03AQF9EJLqIHKenQ/profile-displayphoto-scale_400_400/B4DZ3VVYipG4Ag-/0/1777400647211?e=1779321600&v=beta&t=W8HfdNy1rAEVXsU2-Wms6KofG3KlLvIJt6-ywwCQC9s') 
   } else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src', 'https://avatars.githubusercontent.com/u/225488896?v=4')
   }
 
 }
